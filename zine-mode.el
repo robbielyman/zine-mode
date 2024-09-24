5ef2d253172327c8ba7e58cf0b80a854e6c3f4df
Robbie Lyman <rb.lymn@gmail.com>
2024-09-24 10:42:22 -0400

    feat: add SuperMD

--- zine-mode.el
+++ zine-mode.el
@@ -37,6 +37,8 @@
 (require 'reformatter)
 (require 'css-mode)
 (require 'js)
+(require 'markdown-mode)
+(require 'ziggy-mode)
 
 (defgroup zine-mode nil
   "Tree-sitter powered support for Zine static site code."
@@ -73,6 +75,155 @@
 ;;;###autoload (autoload 'zine-superhtml-format-region "current-file" nil t)
 ;;;###autoload (autoload 'zine-superhtml-format-on-save-mode "current-file" nil t)
 
+(defvar zine-supermd-inline--tresit-range-settings
+  (treesit-range-rules
+   :embed 'html
+   :host 'supermd-inline
+   '((html_tag) @capture)
+
+   :embed 'latex
+   :host 'supermd-inline
+   '((latex_block) @capture))
+  "Tree-sitter injections for supermd-inline.")
+
+(defvar zine-supermd-inline--treesit-font-lock-setting
+  (treesit-font-lock-rules
+   :feature 'verbatim
+   :language 'supermd-inline
+   '([
+      (code_span)
+      (link_title)
+      ] @markdown-code-face)
+
+   :feature 'delimiter
+   :language 'supermd-inline
+   '([
+      (emphasis_delimiter)
+      (code_span_delimiter)
+      ] @font-lock-delimiter-face)
+
+   :feature 'emphasis
+   :language 'supermd-inline
+   '((emphasis) @markdown-italic-face)
+
+   :feature 'strong
+   :language 'supermd-inline
+   '((strong_emphasis) @markdown-bold-face)
+
+   :feature 'url
+   :language 'supermd-inline
+   '([
+      (link_destination)
+      (uri_autolink)
+      ] @markdown-url-face)
+
+   :feature 'reference
+   :language 'supermd-inline
+   '([
+      (link_label)
+      (link_text)
+      (image_description)
+      ] @markdown-reference-face)
+
+   :feature 'escape
+   :language 'supermd-inline
+   '([
+      (backslash_escape)
+      (hard_line_break)
+      ] @font-lock-escape-face)
+
+   :feature 'link-delimiter
+   :language 'supermd-inline
+   '(
+     (image ["!" "[" "]" "(" ")"] @font-lock-delimiter-face)
+     (inline_link ["[" "]" "(" ")"] @font-lock-delimiter-face)
+     (shortcut_link ["[" "]"] @font-lock-delimiter-face)
+     )
+   )
+  "Tree-sitter font-lock settings for supermd-inline.")
+
+(defvar zine-supermd--treesit-range-settings
+  (treesit-range-rules
+   :embed 'html
+   :host 'supermd
+   '((html_block) @capture)
+
+   :embed 'ziggy
+   :host 'supermd
+   '((document . (section . (thematic_break) (_) @capture (thematic_break)))
+     ((minus_metadata) @capture))
+
+   :embed 'supermd_inline
+   :host 'supermd
+   '((inline) @capture))
+  "Tree-sitter injections for supermd.")
+
+(defvar zine-supermd--treesit-font-lock-setting
+  (treesit-font-lock-rules
+   :feature 'title
+   :language 'supermd
+   '((atx_heading (inline) @markdown-header-face)
+     (setext_heading (paragraph) @markdown-header-face))
+
+   :feature 'title-punctuation
+   :language 'supermd
+   '([
+      (atx_h1_marker)
+      (atx_h2_marker)
+      (atx_h3_marker)
+      (atx_h4_marker)
+      (atx_h5_marker)
+      (atx_h6_marker)
+      (setext_h1_underline)
+      (setext_h2_underline)
+      ] @font-lock-punctuation-face)
+
+   :feature 'link-title
+   :language 'supermd
+   '([
+      (link_title)
+      (link_label)
+      ] @markdown-link-title-face)
+
+   :feature 'code-block
+   :language 'supermd
+   '([
+      (indented_code_block)
+      (fenced_code_block)
+      ] @markdown-code-face)
+
+   :feature 'delimiter
+   :language 'supermd
+   '((fenced_code_block_delimiter) @font-lock-delimiter-face)
+
+   :feature 'url
+   :language 'supermd
+   '((link_destination) @markdown-url-face)
+
+   :feature 'list-punctuation
+   :language 'supermd
+   '([
+      (list_marker_plus)
+      (list_marker_minus)
+      (list_marker_star)
+      (list_marker_dot)
+      (list_marker_parenthesis)
+      (thematic_break)
+      ] @markdown-list-face)
+
+   :feature 'other-punctuation
+   :language 'supermd
+   '([
+      (block_continuation)
+      (block_quote_marker)
+      ] @font-lock-punctuation-face)
+
+   :feature 'escape
+   :language 'supermd
+   '((backslash_escape) @font-lock-escape-face)
+   )
+  "Tree-sitter font-lock settings for supermd.")
+
 (defconst zine-mode-superhtml-syntax-table
   (let ((table (make-syntax-table)))
 
@@ -81,7 +232,7 @@
     table))
 
 (defun zine-superhtml--treesit-property-super-extend-p (node)
-  "Check that NODE has text equal to \"super\" or \"extend\""
+  "Check that NODE has text equal to \"super\" or \"extend\"."
   (or (equal (treesit-node-text node) "super")
       (equal (treesit-node-text node) "extend"))
   )
@@ -95,7 +246,8 @@
    :embed 'css
    :host 'superhtml
    '((style_element (raw_text) @capture))
-   ))
+   )
+  "Tree-sitter injections for superhtml.")
 
 (defvar zine-superhtml--treesit-font-lock-setting
   (treesit-font-lock-rules
@@ -177,7 +329,7 @@
   "Tree-sitter font-lock settings for superhtml.")
 
 ;;;###autoload
-(define-derived-mode zine-mode text-mode "Zine"
+(define-derived-mode zine-superhtml-mode text-mode "Zine"
   "A tree-sitter-powered major mode for the Zine static site generator."
   :group 'zine-mode
   :syntax-table zine-mode-superhtml-syntax-table
@@ -191,12 +343,38 @@
                   (string attributes keyword definition)
                   (links variable operator selector property constant query string-interpolation assignment jsx number escape-sequence)
                   (bracket delimiter)))
-    (setq-local treesit-font-lock-settings (append zine-superhtml--treesit-font-lock-setting css--treesit-settings)) ; js--treesit-font-lock-settings))
+    (setq-local treesit-font-lock-settings (append zine-superhtml--treesit-font-lock-setting
+                                                   css--treesit-settings
+                                                   js--treesit-font-lock-settings))
     (setq-local treesit-range-settings zine-superhtml--treesit-range-settings)
     (treesit-major-mode-setup)))
 
 ;;;###autoload
-(add-to-list 'auto-mode-alist '("\\.shtml\\'" . zine-mode))
+(define-derived-mode zine-supermd-mode text-mode "Zine"
+  "A tree-sitter-powered major mode for the Zine static site generator."
+  :group 'zine-mode
+  (when (and (treesit-ready-p 'supermd)
+             (treesit-ready-p 'supermd-inline))
+    (treesit-parser-create 'supermd)
+    (treesit-parser-create 'supermd-inline)
+    (setq-local treesit-font-lock-feature-list
+                '((url comment doctype tag special parse-error function error)
+                  (code-block link-title reference verbatim)
+                  (strong emphasis escape)
+                  (link-delimiter delimiter title-punctuation list-punctuation)))
+    (setq-local treesit-font-lock-settings (append zine-supermd--treesit-font-lock-setting
+                                                   zine-supermd-inline--treesit-font-lock-setting
+                                                   ziggy--treesit-font-lock-setting
+                                                   latex--treesit-font-lock-setting))
+    (setq-local treesit-range-settings (append zine-supermd--treesit-range-settings
+                                               zine-supermd-inline--treesit-range-settings))
+    (treesit-major-mode-setup)))
+
+;;;###autoload
+(add-to-list 'auto-mode-alist '("\\.shtml\\'" . zine-superhtml-mode))
+
+;;;###autoload
+(add-to-list 'auto-mode-alist '("\\.smd\\'" . zine-supermd-mode))
 
 (provide 'zine-mode)
 
